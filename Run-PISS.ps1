091c6af5371950b94892a2fc110fbcbb791492fb
LunixiaLIVE <lunixia@live.com>
2020-03-25 02:15:44 -0500

    Add files via upload

--- Run-PISS.ps1
+++ Run-PISS.ps1
@@ -72,7 +72,7 @@
                         $ENV:INSTALL_KEY = "379CE192D401AB61";
                         $ENV:DEB_DISTRO=$(lsb_release -sc);
                         apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY;
-                        echo "deb https://ookla.bintray.com/debian $DEB_DISTRO main" | tee /etc/apt/sources.list.d/speedtest.list;
+                        tee /etc/apt/sources.list.d/speedtest.list -a "deb https://ookla.bintray.com/debian $DEB_DISTRO main"
                         apt-get update;
                         sudo apt-get install -y speedtest
                     }
